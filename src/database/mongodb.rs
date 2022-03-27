80fd0174fb0f23b6d371e00d89bdc02b6c074302
prongbang <prongbang@gmail.com>
2022-03-27 23:16:08 +0700

    Change pass

--- src/database/mongodb.rs
+++ src/database/mongodb.rs
@@ -1,7 +1,7 @@
 use mongodb::sync::Client;
 
 pub fn new() -> Client {
-    let uri = "mongodb://root:72e827d2-505e-4e4d-bd02-a2d2d05d3f41@192.168.10.102:27017/iotDB?authSource=admin";
+    let uri = "mongodb://root:7fd7583b-f01e-4a5b-869c-01c06ea45cda@192.168.10.102:27017/iotDB?authSource=admin";
     let client = Client::with_uri_str(uri)
         .expect("failed to connect");
     return client;
